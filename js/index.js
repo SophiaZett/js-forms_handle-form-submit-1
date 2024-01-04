045c5cbd3dc583489efbd185342dccc9aa0aa52b
Sophia S. <sophia0791@icloud.com>
2024-01-04 18:05:06 +0100

    add bloody solution

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (banane) => {
+  banane.preventDefault();
+
+  const formData = new FormData(banane.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const firstName = data.firstName;
+  const age = Number(data.age);
+  const badness = Number(data.badness);
+
+  console.log(`The age-badness-sum of ${firstName} is ${age + badness}`);
+
+  banane.target.reset();
+  banane.target.elements.firstName.focus();
+});
